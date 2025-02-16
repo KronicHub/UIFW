b89bbb7db91625fc10f31d420072884a7a6ca0b9
w9x <132112768+windows9xpee@users.noreply.github.com>
2025-02-16 18:27:49 +0100

    base files

---
+++ Library/Library.lua
@@ -0,0 +1,23 @@
+local KRLibrary = {
+    function Window_Add()
+
+    end,
+    function Tab_Add(Window, Name, Icon)
+
+    end,
+    function Section_Add(Tab, Name, Icon)
+
+    end,
+    function Button_Add(Section, Name, Icon)
+
+    end,
+    function Slider_Add(Section, Name, Icon, MaxLength)
+
+    end,
+    function Toggle_Add(Section, Name, Icon)
+
+    end,
+    function Input_Add(Section, Name, Icon)
+
+    end
+}
