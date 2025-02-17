5e58cb752b055da80134c5d9f66e9dc0be52f67b
w9x <132112768+windows9xpee@users.noreply.github.com>
2025-02-17 03:31:47 +0100

    push 6

--- Library/Library.lua
+++ Library/Library.lua
@@ -8,10 +8,24 @@ local KLibrary = {
         local NewGUI = Instance.new('ScreenGui')
         NewGUI.Parent = game.CoreGUI
         NewGUI.IgnoreGuiInset = true
-        NewGUI.Name = Resources.Functions.RandomString()
+        NewGUI.Name = KFunctionResources.RandomString()
+
         local Window = Instance.new('Frame', NewGUI)
         Window.BackgroundColor3 = CosmeticResources.Color.Background
-        Window.Size = UDim2.new()
+        Window.Size = UDim2.new(0,500,0,300)
+        local Window_Corner = Instance.new('UICorner', Window)
+        Window_Corner.Size = UDim.new(0,4)
+
+        local TopBar = Instance.new('Frame', Window)
+        TopBar.Size = UDim2.new(1,0,0,25)
+        TopBar.BackgroundColor3 = KCosmeticResources.Midground
+        local TopBar_Corner = Instance.new('UICorner', TopBar)
+        TopBar_Corner.Size = UDim.new(0,4)
+        local Topbar_Bottom = Instance.new('Frame', TopBar)
+        TopBar_Bottom.Size = UDim2.new(1,0,0,25)
+        TopBar_Bottom.BackgroundColor3 = KCosmeticResources.Midground
+        
+        m
     end,
     function Tab_Add(Window, Name, Icon)
 
--- Library/Resources/Cosmetic.lua
+++ Library/Resources/Cosmetic.lua
@@ -1,6 +1,6 @@
-local CosmeticResources = {
+local KCosmeticResources = {
     local Color = {
-        Background = Color3.fromRGB(19,21,20)
+        Background = Color3.fromRGB(20,20,20)
         Midground = Color3.fromRGB(19,21,20)
         Foreground = Color3.fromRGB(19,21,20)
         Accent = Color3.fromRGB(19,21,20)
